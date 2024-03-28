9ac22883e22ae1c5261dcab4f561349c6f739a4b
Emely - Desenvolvimento 06 <desenvolvimento06@pauta.com.br>
2024-03-28 16:27:56 -0300

    feat: classes for a order

---
+++ balcao/Entities/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balcao.Entities
+{
+    class Cliente
+    {
+       public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+        public Cliente() { }
+        public Cliente(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return "Cliente: " + Name + ", (" + BirthDate.ToString("dd/MM/yyyy") +
+                ") - " + Email;
+        }
+    }
+}
